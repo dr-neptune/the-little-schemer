a889f269674290885aeac3628d78858e120d3130
dr-neptune <mrose4@worcester.edu>
2023-01-15 13:28:21 -0500

    (define cookies (bake (mix (map mix '(dry-ingredients wet-ingredients chocolate)))))

--- chapter7.rkt
+++ chapter7.rkt
@@ -110,9 +110,146 @@
 (define (match/intersect s1 s2)
   (match s1
     ['() '()]
-    [(list* (? (λ (v) (member? v s2))) tail)
-     (cons (first s1) (match/intersect tail s2))]
+    [(list* (? (curryr member? s2) head) tail)
+     (cons head (match/intersect tail s2))]
     [_ (match/intersect (rest s1) s2)]))
 
 (match/intersect '(stewed tomatoes and macaroni)
                  '(macaroni and cheese))
+
+(define (union s1 s2)
+  (cond [(null? s1) s2]
+        [(member? (first s1) s2)
+         (union (rest s1) s2)]
+        [else (union (rest s1) (cons (first s1) s2))]))
+
+(union '(stewed tomatoes and macaroni casserole)
+       '(macaroni and cheese))
+
+(define (match/union s1 s2)
+  (match s1
+    ['() s2]
+    [(list* (? (curryr member? s2) head) tail)
+     (match/union tail s2)]
+    [_ (match/union (rest s1) (cons (first s1) s2))]))
+
+(match/union '(stewed tomatoes and macaroni casserole)
+             '(macaroni and cheese))
+
+
+(define (difference s1 s2)
+  (cond [(null? s1) '()]
+        [(member? (first s1) s2)
+         (difference (rest s1) s2)]
+        [else (cons (first s1)
+                    (difference (rest s1) s2))]))
+
+
+(difference '(stewed tomatoes and macaroni casserole)
+            '(macaroni and cheese))
+
+(define (match/difference s1 s2)
+  (match s1
+    ['() '()]
+    [(list* (? (compose not (curryr member s2)) head) tail)
+     (cons head (match/difference tail s2))]
+    [_ (match/difference (rest s1) s2)]))
+
+(match/difference '(stewed tomatoes and macaroni casserole)
+                  '(macaroni and cheese))
+
+(define (book/intersectall l-set)
+  (cond [(null? (cdr l-set)) (car l-set)]
+        [else
+         (intersect (car l-set)
+                    (intersectall (cdr l-set)))]))
+
+(book/intersectall '((a b c) (c a d e) (e f g h a b)))
+
+(define (intersectall l-set)
+  (foldl intersect (first l-set) (rest l-set)))
+
+(intersectall '((a b c) (c a d e) (e f g h a b)))
+
+(define (atom? x)
+  (and (not (pair? x))
+       (not (null? x))))
+
+(define (lat? ls)
+  (andmap atom? ls))
+
+(define (match/intersectall l-set)
+  (match l-set
+    [(list a) #:when (lat? a) a]
+    [_ (intersect (first l-set)
+                  (match/intersectall (rest l-set)))]))
+
+(match/intersectall '((a b c) (c a d e) (e f g h a b)))
+
+(define (a-pair? x)
+  (eq? (length x) 2))
+
+(a-pair? '(pear pear))
+(a-pair? '(3 7))
+(a-pair? '((2) (pair)))
+(a-pair? '(full (house)))
+(a-pair? '(very (full (house))))
+(a-pair? '(very very (full (house))))
+
+(define (match/a-pair? x)
+  (match x
+    [(list a b) #t]
+    [_ #f]))
+
+(match/a-pair? '(pear pear))
+(match/a-pair? '(3 7))
+(match/a-pair? '((2) (pair)))
+(match/a-pair? '(full (house)))
+(match/a-pair? '(very (full (house))))
+(match/a-pair? '(very very (full (house))))
+
+
+(define (firsts ls)
+  (cond [(null? ls) '()]
+        [else (cons (first (first ls))
+                    (firsts (rest ls)))]))
+
+
+(define (fun? rel)
+  (set? (firsts rel)))
+
+(fun? '((8 3)(4 2)(7 6)(6 2)(3 4)))
+
+(define (revrel rel)
+  (map reverse rel))
+
+(define (build s1 s2)
+  (cons s1 (cons s2 '())))
+
+(define (book/revrel rel)
+  (cond [(null? rel) '()]
+        [else (cons (build (second (first rel))
+                           (first (first rel)))
+                    (book/revrel (rest rel)))]))
+
+(revrel '((8 a) (pumpkin pie) (got sick)))
+(book/revrel '((8 a) (pumpkin pie) (got sick)))
+
+(define (revpair pair)
+  (build (second pair) (first pair)))
+
+
+(define (book/revised/revrel rel)
+  (cond [(null? rel) '()]
+        [else (cons (revpair (car rel))
+                    (book/revised/revrel (rest rel)))]))
+
+(book/revised/revrel '((8 a) (pumpkin pie) (got sick)))
+
+(define (seconds ls)
+  (cond [(null? ls) '()]
+        [else (cons (second (first ls))
+                    (seconds (rest ls)))]))
+
+(define (fullfun? fun)
+  (set? (seconds fun)))
