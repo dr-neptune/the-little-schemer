c2f1260f8fbadcdb085b7081ba31160a2f84581b
dr-neptune <mrose4@worcester.edu>
2023-01-16 17:59:18 -0500

    (eq? 'continuations 'collectors)

--- chapter8.rkt
+++ chapter8.rkt
@@ -58,3 +58,191 @@
 (rember-f = 5 '(6 2 5 3))
 (rember-f eq? 'jelly '(jelly beans are good))
 (rember-f equal? '(pop corn) '(lemonade (pop corn) and (cake)))
+
+(define eq?-c
+  (λ (a)
+    (λ (x)
+      (eq? x a))))
+
+(define (refined/rember-f test?)
+  (lambda (s l)
+    (cond [(null? l) '()]
+          [(test? (first l) s)
+           ((refined/rember-f test?) s (rest l))]
+          [else (cons (first l) ((refined/rember-f test?) s (rest l)))])))
+
+
+((refined/rember-f =) 5 '(6 2 5 3))
+((refined/rember-f eq?) 'jelly '(jelly beans are good))
+((refined/rember-f equal?) '(pop corn) '(lemonade (pop corn) and (cake)))
+((refined/rember-f eq?) 'tuna '(shrimp salad and tuna salad))
+
+(define (insertL-f test?)
+  (λ (new old l)
+    (cond [(null? l) '()]
+          [(test? (first l) old)
+           (cons new (cons old (rest l)))]
+          [else (cons (first l)
+                      ((insertL-f test?) new old (rest l)))])))
+
+(define (insertR-f test?)
+  (λ (new old l)
+    (cond [(null? l) '()]
+          [(test? (first l) old)
+           (cons old (cons new (rest l)))]
+          [else (cons (first l)
+                      ((insertR-f test?) new old (rest l)))])))
+
+((insertL-f eq?) 8 5 '(1 2 3 4 5 6 7))
+((insertR-f eq?) 8 5 '(1 2 3 4 5 6 7))
+
+(define (insert-g side test?)
+  (λ (new old l)
+    (cond [(null? l) '()]
+          [(test? (first l) old)
+           (if (equal? side 'L)
+               (cons new (cons old ((insert-g 'L test?) new old (rest l))))
+               (cons old (cons new ((insert-g 'R test?) new old (rest l)))))]
+          [else (cons (first l)
+                      ((insert-g side test?) new old (rest l)))])))
+
+((insert-g 'L eq?) 8 5 '(1 2 3 4 5 6 7))
+((insert-g 'R eq?) 8 5 '(1 2 3 4 5 6 7))
+
+(define (seqL a b c)
+  (cons a (cons b c)))
+
+(define (seqR a b c)
+  (cons b (cons a c)))
+
+
+(define (helper/insert-g side test?)
+  (λ (new old l)
+    (cond [(null? l) '()]
+          [(test? (first l) old)
+           (if (equal? side 'L)
+               (seqL new old ((insert-g 'L test?) new old (rest l)))
+               (seqR new old ((insert-g 'L test?) new old (rest l))))]
+          [else (cons (first l)
+                      ((insert-g side test?) new old (rest l)))])))
+
+
+((insert-g 'L eq?) 8 5 '(1 2 3 4 5 6 7))
+((insert-g 'R eq?) 8 5 '(1 2 3 4 5 6 7))
+
+(define (book/insert-g seq)
+  (λ (new old l)
+    (cond [(null? l) '()]
+          [(eq? (car l) old)
+           (seq new old (cdr l))]
+          [else (cons (car l)
+                      ((book/insert-g seq) new old (cdr l)))])))
+
+(define insertL
+  (book/insert-g seqL))
+
+(define insertR
+  (book/insert-g seqR))
+
+(insertL 2 5 '(1 2 3 4 5))
+(insertR 2 5 '(1 2 3 4 5))
+
+(define (subst new old l)
+  (cond [(null? l) '()]
+        [(eq? (first l) old)
+         (cons new (rest l))]
+        [else (cons (first l) (subst new old (rest l)))]))
+
+(subst 'almond 'peanut '(peanut butter and grape flavored jelly))
+
+(define (seqS new old l)
+  (cons new l))
+
+(define book/subst
+  (book/insert-g seqS))
+
+(book/subst 'almond 'peanut '(peanut butter and marmalade))
+
+(define (new/rember a l)
+  ((book/insert-g (λ (new old l) l)) #f a l))
+
+(new/rember 'peanut '(peanut butter and marmalade))
+
+(define (↑ m n)
+  (cond [(eq? n 0) 1]
+        [else
+         (* m (↑ m (sub1 n)))]))
+
+(define (operator aexp) (car aexp))
+
+(define (atom-to-function x)
+  (match x
+    ['+ +]
+    ['* *]
+    [_ ↑]))
+
+(atom-to-function (operator '(+ 5 3)))
+
+(define (value nexp)
+  (cond [(number? nexp) nexp]
+        [else ((atom-to-function (operator nexp))
+               (value (second nexp))
+               (value (third nexp)))]))
+
+(value '(+ 5 3))
+
+(define (multirember-f pred?)
+  (λ (a lat)
+    (cond [(null? lat) '()]
+          [(pred? (car lat) a)
+           ((multirember-f pred?) a (cdr lat))]
+          [else
+           (cons (car lat) ((multirember-f pred?) a (cdr lat)))])))
+
+((multirember-f eq?) 'peanut '(peanut brittle and peanut toffee))
+((multirember-f eq?) 'tuna '(shrimp salad tuna salad and tuna))
+
+(define multirember-eq? (multirember-f eq?))
+
+(define eq?-tuna (eq?-c 'tuna))
+
+(define (multiremberT test? lat)
+  (cond [(null? lat) '()]
+        [(test? (first lat))
+         (multiremberT test? (rest lat))]
+        [else
+         (cons (first lat)
+               (multiremberT test? (rest lat)))]))
+
+(multiremberT eq?-tuna '(shrimp salad tuna salad and tuna))
+
+(define (multirember&co a lat col)
+  (cond [(null? lat) (begin
+                       (displayln (format "hitting end: ~a ~a ~a" a lat col))
+                       (col '() '()))]
+        [(eq? (car lat) a)
+         (begin
+           (displayln (format "found a: ~a ~a ~a" a lat col))
+           (multirember&co a (cdr lat) (λ (newlat seen)
+                                       (col newlat
+                                            (cons (car lat) seen)))))]
+        [else
+         (begin
+           (displayln (format "did not find a: ~a ~a ~a" a lat col))
+           (multirember&co a (cdr lat) (λ (newlat seen)
+                                         (col (cons (car lat) newlat) seen))))]))
+
+(define a-friend (λ (x y) (null? y)))
+
+(multirember&co 'tuna '(strawberries tuna and swordfish) a-friend)
+
+(multirember&co 'tuna '() a-friend)
+(multirember&co 'tuna '(tuna) a-friend)
+
+(define (new-friend newlat seen)
+  (a-friend newlat (cons 'tuna seen)))
+
+(multirember&co 'tuna '(tuna) new-friend)
+(multirember&co 'tuna '(and tuna) new-friend)
+
+(multirember&co 'tuna '(strawberries tuna and swordfish) (λ (x y) (length x)))
