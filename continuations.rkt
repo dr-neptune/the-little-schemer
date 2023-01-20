c1251e279cd562caf3ef5f5577c048c6664aa225
dr-neptune <mrose4@worcester.edu>
2023-01-20 10:30:06 -0500

    error handling

--- continuations.rkt
+++ continuations.rkt
@@ -94,3 +94,99 @@
   (assert (< b a))
   (display (list a b c))
   (newline))
+
+;; SAT-solving with amb
+(define (implies a b) (or (not a) b))
+
+
+(define-syntax sat-solve
+  (syntax-rules (and or implies not)
+    ((_ formula body)
+     ; =>
+     (sat-solve formula body formula))
+
+    ((_ (not phi) body assertion)
+     ; =>
+     (sat-solve phi body assertion))
+
+    ((_ (and phi) body assertion)
+     ; =>
+     (sat-solve phi body assertion))
+
+    ((_ (and phi1 phi2 ...) body assertion)
+     ; =>
+     (sat-solve phi1 (sat-solve (and phi2 ...) body assertion)))
+
+    ((_ (or phi) body assertion)
+     ; =>
+     (sat-solve phi body assertion))
+
+    ((_ (or phi1 phi2 ...) body assertion)
+     ; =>
+     (sat-solve phi1 (sat-solve (or phi2 ...) body assertion)))
+
+    ((_ (implies phi1 phi2) body assertion)
+     ; =>
+     (sat-solve phi1 (sat-solve phi2 body assertion)))
+
+    ((_ #t body assertion)
+     ; =>
+     body)
+
+    ((_ #f body assertion)
+     ; =>
+     (fail))
+
+    ((_ v body assertion)
+     (let ((v (amb (list #t #f))))
+       (if (not assertion)
+           (fail)
+           body)))))
+
+
+(display
+ (sat-solve (and (implies a (not b)) (not a) c) (list a b c)))
+
+;; concensus theorem
+;; https://math.stackexchange.com/a/2435421
+(display
+ (sat-solve (implies (and (or a b) (or (not b) c))
+                     (and (or a b) (or (not b) c) (or a c))) (list a b c)))
+
+;; Exception Handling
+
+; exception-stack : list[continuation]
+(define exception-stack '())
+
+; (try exp ... catch catch-procedure) runs
+; exp ..., and invokes catch-procedure with
+; the value passed to throw
+(define-syntax try
+  (syntax-rules (catch)
+    ((_ exp ... catch proc)
+     ; =>
+     (let ((cc (current-continuation)))
+       (cond
+         ((procedure? cc)
+          (dynamic-wind (λ () (set! exception-stack (cons cc exception-stack)))
+                        (λ () exp ...)
+                        (λ () (set! exception-stack (cdr exception-stack)))))
+         ((pair? cc)
+          (proc (cadr cc))))))))
+
+(define (throw exception-value)
+  (let ((handler (car exception-stack)))
+    (handler (list 'exception exception-value))))
+
+(try (try (throw 'foo)
+          catch
+          (λ (exn)
+            (display (format "got inner exception ~a" exn))
+            (newline)
+            (throw 'bar)))
+     catch
+     (λ (exn)
+       (display (format "got outer exception ~a" exn))
+       (newline)))
+
+;; Generators
